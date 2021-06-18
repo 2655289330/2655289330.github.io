d73e6ac25e50255f451eb215c4512a39188599d4
2655289330 <86000154+2655289330@users.noreply.github.com>
2021-06-18 18:11:12 +0800

    Update thumbnail.js

--- thumbnail.js
+++ thumbnail.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
     let targetImage = document.querySelector("#smart-image");
     targetImage.addEventListener("click", function() {
-         targetImage.classList.remove("small");
+         targetImage.classList.add("small");
     
 
     });
